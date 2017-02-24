123dbc33e201b90aab0a1d6e55756e1f26d74038
TonIveSat <tonny.sato@gmail.com>
2017-02-25 00:58:41 +0100

    FadePanel

---
+++ Assets/Scripts/FadeIn.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class FadeIn : MonoBehaviour
+{
+    public float FadeSpeed = 1;
+
+    private float alpha = 1;
+
+    private Image panelImage;
+
+    // Use this for initialization
+    void Start ()
+    {
+        panelImage = GetComponent<Image>();
+        panelImage.color = new Color(0, 0, 0, alpha);
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        alpha -= Time.deltaTime * FadeSpeed;
+        if (alpha < 0)
+        {
+            gameObject.SetActive(false);
+        }
+        panelImage.color = new Color(0, 0, 0, alpha);
+    }
+}
