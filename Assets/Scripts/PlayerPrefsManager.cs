8e363e8695cae9f6a87ece2c24ad1a67231a7ff2
TonIveSat <tonny.sato@gmail.com>
2017-02-28 22:15:43 +0100

    Player prefs 2

--- Assets/Scripts/PlayerPrefsManager.cs
+++ Assets/Scripts/PlayerPrefsManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerPrefsManager : MonoBehaviour
 {
@@ -23,5 +24,48 @@ public class PlayerPrefsManager : MonoBehaviour
     public static float GetMasterVolume()
     {
         return PlayerPrefs.GetFloat(MASTER_VOLUME_KEY);
+    }
+
+    public static void UnlockLevel(int level)
+    {
+        if (level < SceneManager.sceneCountInBuildSettings - 1)
+        {
+            PlayerPrefs.SetInt(LEVEL_KEY + level, 1); // 1 is true
+        }
+        else
+        {
+            Debug.LogError("Trying to unlock level that is not in build order.");
+        }
+    }
+
+    public static bool IsLevelUnlocked(int level)
+    {
+        if (level < SceneManager.sceneCountInBuildSettings - 1)
+        {
+            return PlayerPrefs.GetInt(LEVEL_KEY + level) == 1; 
+        }
+        else
+        {
+            Debug.LogError("Trying to adress level that is not in build order.");
+        }
+
+        return false;
+    }
+
+    public static void SetDifficulty(float difficulty)
+    {
+        if (difficulty > 1f || difficulty < 0)
+        {
+            Debug.LogError("Difficulty out of range: " + difficulty.ToString());
+        }
+        else
+        {
+            PlayerPrefs.SetFloat(DIFFICULTY_KEY, difficulty);
+        }
+    }
+
+    public static float GetDifficulty()
+    {
+        return PlayerPrefs.GetFloat(DIFFICULTY_KEY);
     }
 }
