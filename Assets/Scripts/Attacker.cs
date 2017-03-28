e04c6134318293c20bec5805f9466e4e6e14ff70
TonIveSat <tonny.sato@gmail.com>
2017-03-28 22:48:11 +0200

    attacker walk speed

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Attacker : MonoBehaviour
+{
+    [Range(-1, 1.5f)]
+    public float WalkSpeed;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(Vector3.left * WalkSpeed * Time.deltaTime);
+	}
+}
