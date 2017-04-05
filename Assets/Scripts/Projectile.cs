e674136f76f31d0bf103dc5e229fe2af789d6faa
TonIveSat <tonny.sato@gmail.com>
2017-04-05 18:33:38 +0200

    Lecture 160

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float Speed;
+
+    public float Damage;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(Vector3.right * Speed * Time.deltaTime);
+	}
+}
