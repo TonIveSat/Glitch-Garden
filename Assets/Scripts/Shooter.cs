8b2d3ba2b723c6cd6361662d406a6347f21c8972
TonIveSat <tonny.sato@gmail.com>
2017-04-27 21:31:36 +0200

    Autocreate projectile parent

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,24 +4,30 @@ public class Shooter : MonoBehaviour
 {
     public GameObject ProjectileToBeShot;
 
-    public GameObject ProjectileParent;
-
     public GameObject Gun;
+
+    private GameObject ProjectileParent;
+
+    // Use this for initialization
+    void Start ()
+    {
+        //gun = gameObject.get
+        ProjectileParent = GameObject.Find("Projectiles");
+        if (ProjectileParent == null)
+        {
+            ProjectileParent = new GameObject("Projectiles");
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 
     private void Fire()
     {
         var newProjectile = Instantiate(ProjectileToBeShot, ProjectileParent.transform) as GameObject;
         newProjectile.transform.position = Gun.transform.position;
     }
-
-    // Use this for initialization
-    void Start ()
-    {
-		//gun = gameObject.get½1
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
